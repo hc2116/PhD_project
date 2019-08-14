2f82d66e969102a808b879909cb0248c204c8b06
hc2116 <henry.clausen@ed.ac.uk>
2019-08-14 13:27:57 +0200

    tunnels

--- Data/ftp_double_testing_sshtunnel/scripts/ssh-tunnel-creation.sh
+++ Data/ftp_double_testing_sshtunnel/scripts/ssh-tunnel-creation.sh
@@ -2,6 +2,7 @@
 SERVER="172.16.238.5"
 CLIENT="172.16.238.20"
 TUNNEL="172.16.238.10"
+FTP_SERVER="172.16.238.11"
 USER="root"
 PASS="root"
 # Check if necessary to scan host
@@ -12,7 +13,7 @@ PASS="root"
 
 
 echo "Creating tunnel "
-sshpass -p $PASS ssh -4 -o StrictHostKeyChecking=no -L 7777:localhost:22 root@$SERVER -f -N
+sshpass -p $PASS ssh -4 -o StrictHostKeyChecking=no -L 7777:$FTP_SERVER:21 root@$SERVER -f -N
 sleep 1
 sshpass -p $PASS ssh -4 -o StrictHostKeyChecking=no -R 7777:localhost:7777 root@$CLIENT -f -N
 #sshpass -p $PASS ssh -R 7777:$SERVER:22 root@$CLIENT -f -N
