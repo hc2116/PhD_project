0fe9e4dd49e3bb5ce2e3fd2f54a0f3c7760f1f04
hc2116 <henry.clausen@ed.ac.uk>
2019-08-14 12:47:55 +0200

    tunnels

--- Data/double_sshtunnel/scripts/keyscanner.sh
+++ Data/double_sshtunnel/scripts/keyscanner.sh
@@ -2,7 +2,7 @@
 SERVER="172.16.238.5"
 CLIENT="172.16.238.20"
 TUNNEL="172.16.238.10"
-LOCALHOST="localhost:7777"
+LOCALHOST="[localhost]:7777"
 # Check if necessary to scan host
 KNOWNHOSTFILE=~/.ssh/known_hosts
 ssh-keyscan -H $SERVER $CLIENT $TUNNEL $LOCALHOST >> ~/.ssh/known_hosts
